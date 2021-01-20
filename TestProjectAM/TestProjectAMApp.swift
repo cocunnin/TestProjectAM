41f9a96ee0646ed80cc79e44e29d84224e3da60a
a.m.student <cy.yahoo.om>
2021-01-20 08:51:46 -0600

    Initial Commit

---
+++ TestProjectAM/TestProjectAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectAMApp.swift
+//  TestProjectAM
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
